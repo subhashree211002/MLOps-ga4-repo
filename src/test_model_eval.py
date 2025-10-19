f9bd5a09eb1b96d6b908fb943fac490d1f26ec4f
Subhashree <subhashreemanogaran666@gmail.com>
2025-10-19 23:33:28 +0530

    Update test_model_eval.py

--- src/test_model_eval.py
+++ src/test_model_eval.py
@@ -5,6 +5,21 @@ from joblib import load
 def test_model_evaluation(tmp_path):
     # Assuming model.pkl & test data are fetched via DVC
     model = load("models/model.pkl")
+
+    df = pd.DataFrame({
+        'sepal_length': [5.1, 4.9],
+        'sepal_width': [3.5, 3.0],
+        'petal_length': [1.4, 1.4],
+        'petal_width': [0.2, 0.2],
+        'species': ['setosa', 'setosa']
+    })
+    
+    X = df.drop("species", axis=1)
+    y = df["species"]
+
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    
     accuracy = evaluate_model(model)
     assert 0 <= accuracy <= 1
 
